d3fd928ab99fa787a1e734e2ce2e8cd292840d98
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-10 17:01:37 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:weather/weather.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() {
+  String key = "30d288037340bc186f3c77d4ad7d435a";
+}
